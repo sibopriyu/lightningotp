99f65539810b5aeba4b5d62c5208dcf5a0766687
sibopriyu <gamingbadboy924@gmail.com>
2025-07-05 12:08:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def create_order():
     })
     return jsonify(payment)
 
-if name == "main":
+if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
